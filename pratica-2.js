14b958485626a4e067ce0964c4a5a8d8da5607af
Thiagoramosmorato <111910722+Thiagoramosmorato@users.noreply.github.com>
2022-08-26 15:18:23 -0300

    Update pratica-2.js

---
+++ pratica-2.js
@@ -0,0 +1,85 @@
+// Este é um comentário no código. Comentários não são cumpridos, eles servem pra gente deixar mensagem
+// para outras pessoas que sabem o código. Agora vamos para o exercício.
+
+// Analisar os 0 comandos 'se' seguir e pré-ver qual mensagem será exibida na tela.
+// Fique à vontade de rodar o código para verificar se suas respostas estão corretas.
+
+seja   a   =   10
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if   (  a   ==   10  )   {
+    consolo  .  log  (  "Verdade! O valor"  ,   a  ,   "é igual a 10"  )  ;
+}   senão  {
+    consolo  .  log  (  "Falso! O valor"  ,   a  ,   "não é igual a 10"  )  ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+se   (  a   >   10  )  {
+    consolo  .  log  (  "Verdade! O valor"  ,   a  ,   "é maior que 10"  )  ;
+}   senão  {
+    consolo  .  log  (  "Falso! O valor"  ,   a  ,   "não é maior 10"  )  ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if   (  10   <=   a  )   {
+    consolo  .  log  (  "Verdade! O valor"  ,   a  ,   "é maior ou igual a 10"  )  ;
+}   senão  {
+    consolo  .  log  (  "Falso! O valor"  ,   a  ,   "não é maior ou igual a 10"  )  ;
+}
+
+seja   b   =   verdadeiro
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if   (  b   ==   verdadeiro  )   {
+    consolo  .  log  (  "Verdade! O valor"  ,   b  ,   "é igual a true"  )  ;
+}   senão  {
+    consolo  .  log  (  "Falso! O valor"  ,   b  ,   "não é igual a true"  )  ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+se   (  b  )  {
+    consolo  .  log  (  "Verdade! O valor"  ,   b  ,   "é considerado como verdade"  )  ;
+}   senão  {
+    consolo  .  log  (  "Falso! O valor"  ,   b  ,   "não é considerado como verdade"  )  ;
+}
+
+seja   c   =   [  ]
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+se   (  c  )  {
+    consolo  .  log  (  "Verdade! O valor"  ,   c  ,   "é considerado como verdade"  )  ;
+}   senão  {
+    consolo  .  log  (  "Falso! O valor"  ,   c  ,   "não é considerado como verdade"  )  ;
+}
+
+seja   d   =   [  1  ,  2  ,  3  ,  4  ]
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+se   (  d  )  {
+    consolo  .  log  (  "Verdade! O valor"  ,   d  ,   "é considerado como verdade"  )  ;
+}   senão  {
+    consolo  .  log  (  "Falso! O valor"  ,   d  ,   "não é considerado como verdade"  )  ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if   (  d  [  0  ]   ==   1  )   {
+    consolo  .  log  (  "Verdade! O valor"  ,   d  [  0  ]  ,   "do elemento do índice 0 da variável 'd'é igual a 1"  )  ;
+}   senão  {
+    consolo  .  log  (  "Falso! O valor"  ,   d  [  0  ]  ,   "do elemento do índice 0 da variável 'd'não é igual a 1"  )  ;
+}
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if   (  d  [  1  ]   ==   1  )   {
+    consolo  .  log  (  "Verdade! O valor"  ,   d  [  1  ]  ,   "do elemento do índice 1 da variável 'd'é igual a 1"  )  ;
+}   senão  {
+    consolo  .  log  (  "Falso! O valor"  ,   d  [  1  ]  ,   "do elemento do índice 1 da variável 'd'não é igual a 1"  )  ;
+}
+
+deixe   e   =   "Nome"
+
+// Escreva depois dos ':' se vai entrar no 'if' ou no 'else' ('if' se entrar no 'if' e 'else' se entrar no 'else'):
+if   (  typeof  (  e  )   ==   'string'  )   {
+    consolo  .  log  (  "Verdade! O tipo de dado da variável 'e' é uma string"  )  ;
+}   senão  {
+    consolo  .  log  (  "Falso! O tipo de dado da variável 'e' não é uma string"  )  ;
+}
